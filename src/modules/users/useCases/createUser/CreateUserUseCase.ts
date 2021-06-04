abe2082c03a1f96fecc4f7153c2e9a8933d32607
José Assis de Menezes Neto <netomenezesucg@gmail.com>
2021-06-04 13:08:45 -0300

    Initial commit

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,26 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("Already exists a user with same email");
+    }
+
+    return this.usersRepository.create({
+      name,
+      email,
+    });
+  }
+}
+
+export { CreateUserUseCase };
